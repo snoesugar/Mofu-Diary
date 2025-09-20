5dabbbaa5a776e3a1640012244d1abf92f15d779
middle-aged-Weiwei <chinweihsu83221@gmail.com>
2025-09-20 23:51:34 +0800

    改背景圖路徑第六版

--- main.js
+++ main.js
@@ -5,15 +5,7 @@ import './assets/js/air-datepicker-setting';
 
 console.log('Hello world');
 
-import bg1   from './assets/images/bg/bg1.png';
-import e9    from './assets/images/overview/Ellipse9.png';
-import e8    from './assets/images/overview/Ellipse8.png';
-import e8big from './assets/images/overview/Ellipse8-big.png';
 
-const root = document.documentElement;
-root.style.setProperty('--bg-scene-img', `url("${bg1}")`);
-root.style.setProperty('--save-bg-img',  `url("${e9}"), url("${e8}")`);
-root.style.setProperty('--tasks-bg-img', `url("${e8big}")`);
 
 
 
